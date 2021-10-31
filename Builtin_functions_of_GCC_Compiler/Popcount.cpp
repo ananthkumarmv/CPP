bfaf4714d3f642a90b93b78d5c0ddc6b8b57bbd9
Ananth <mvananthkumar@gmail.com>
2021-10-31 23:18:34 +0530

    Update Popcount.cpp

--- Builtin_functions_of_GCC_Compiler/Popcount.cpp
+++ Builtin_functions_of_GCC_Compiler/Popcount.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 //_builtin_popcount(x): This function is used to count 
-//the number of one’s(set bits) in an integer.
+//the number of oneâ€™s(set bits) in an integer
 
 //4-100-1
 //5=101-2
