826452c7b414e12919c614dcb97922a87e21e211
Ananth <mvananthkumar@gmail.com>
2022-01-18 13:15:51 +0530

    Create floor.cpp

---
+++ Functions/floor.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	
+	cout << "Floor is : " << floor(2.3) << endl;
+    cout << "Floor is : " << floor(-2.3) << endl;
+    return 0;
+}
+
+
