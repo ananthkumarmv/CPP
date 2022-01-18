c6b8d97e038bc1c3c5e51abce654d812f2390307
Ananth <mvananthkumar@gmail.com>
2022-01-19 00:17:06 +0530

    Create ceil.cpp

---
+++ Functions/ceil.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	
+	cout << " Ceil is : " << ceil(2.3) << endl;
+    cout << " Ceil is : " << ceil(-2.3) << endl;
+    
+    return 0;
+}
+
+
