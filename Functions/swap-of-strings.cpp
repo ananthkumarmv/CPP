558e1969b1fa3cb58794ec79061b938940102034
Ananth <mvananthkumar@gmail.com>
2022-01-20 00:23:51 +0530

    Create swap-of-strings.cpp

---
+++ Functions/swap-of-strings.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+int main()
+{
+    string a = "Ananth";
+    string b = "Amith";
+    cout << "Value of a before: " << a << endl;
+    cout << "Value of b before: " << b << endl;
+    swap(a, b);
+    cout << "Value of a now: " << a << endl;
+    cout << "Value of b now: " << b << endl;
+    return 0;
+}
+
