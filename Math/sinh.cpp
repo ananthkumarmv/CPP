acf8035b90654cf8b01c6baed69e11aa07446136
Ananth <mvananthkumar@gmail.com>
2022-03-31 18:48:31 +0530

    Create sinh.cpp

---
+++ Math/sinh.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	double x = 4.1;
+	
+	double result = sinh(x);
+	cout<<"sinh(4.1) = "<<result<<endl;
+	
+	// x in Degrees
+	double xDegrees = 90;
+	x = xDegrees * 3.142 / 180;
+	
+	result = sinh(x);
+	
+	cout<<"sinh(90 degrees) = " << result << endl;
+
+
+    return 0;
+}
+
+
