251ea9739dd26818dd5b99de9ac80f9403315e7d
Ananth <mvananthkumar@gmail.com>
2022-03-31 18:48:38 +0530

    Create sinh3.cpp

---
+++ Math/sinh/sinh3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	double x = 3000.0;
+	
+	double result = sinh(x);
+	cout << "sinh(x) = " << result << endl;
+
+
+    return 0;
+}
+
+
